21340b657d42cee6211f7b24b49869370f4bbbda
Carol Lee <CLee@thrillworks.com>
2020-10-22 22:12:50 -0400

    Setup production build and server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'build');
+
+app.use(express.static(publicPath));
+
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+app.listen(8000, () => {
+  console.log('Server is up.');
+});
