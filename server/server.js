d8ba993f498b59ff8ba3a76d03d012abdf7d2c96
Carol Lee <CLee@thrillworks.com>
2020-10-22 23:10:18 -0400

    Added get method

--- server/server.js
+++ server/server.js
@@ -5,9 +5,9 @@ const publicPath = path.join(__dirname, '..', 'build');
 
 app.use(express.static(publicPath));
 
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(8000, () => {
   console.log('Server is up.');
